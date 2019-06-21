3cc85fca842d3cb8a7a619286b741eeaae6081a1
Darin <darinevanow@gmail.com>
2019-06-21 16:16:21 -0700

    First attempt at problem4.

---
+++ P2/problem_4.py
@@ -0,0 +1,41 @@
+def sort_012(input_list):
+    """
+    Given an input array consisting on only 0, 1, and 2, sort the array in a single traversal.
+
+    Args:
+       input_list(list): List to be sorted
+    """
+    i = 0
+    j = 0
+    k = len(input_list) - 1
+
+    while j <= k:
+        if input_list[j] < 1:
+            temp = input_list[i]
+            input_list[i] = input_list[j]
+            input_list[j] = temp
+            i += 1
+            j += 1
+        elif input_list[j] > 1:
+            temp = input_list[j]
+            input_list[j] = input_list[k]
+            input_list[k] = temp
+            k -= 1
+        else:
+            j += 1
+
+    return input_list
+
+
+def test_function(test_case):
+    sorted_array = sort_012(test_case)
+    print(sorted_array)
+    if sorted_array == sorted(test_case):
+        print("Pass")
+    else:
+        print("Fail")
+
+
+test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
+test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
+test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
