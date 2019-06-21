9d487afaa56c024f71b56359061c8d05f9ba404b
Darin <darinevanow@gmail.com>
2019-06-21 14:54:59 -0700

    First attempt at problem1.

---
+++ P2/problem_1.py
@@ -0,0 +1,25 @@
+def sqrt(number):
+    """
+    Calculate the floored square root of a number
+
+    Args:
+       number(int): Number to find the floored squared root
+    Returns:
+       int: Floored Square Root
+    """
+    if number is 0 or number is 1:
+        return number
+    else:
+        i = 1;
+        result = 1;
+        while (result <= number):
+            i += 1
+            result = i * i
+
+    return i - 1
+
+print ("Pass" if  (3 == sqrt(9)) else "Fail")
+print ("Pass" if  (0 == sqrt(0)) else "Fail")
+print ("Pass" if  (4 == sqrt(16)) else "Fail")
+print ("Pass" if  (1 == sqrt(1)) else "Fail")
+print ("Pass" if  (5 == sqrt(27)) else "Fail")
