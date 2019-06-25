951b1e774e46fdfecbe36938d140620a034c0407
Darin <darinevanow@gmail.com>
2019-06-25 13:47:12 -0700

    Simplified problem6.

--- P2/problem_6.py
+++ P2/problem_6.py
@@ -1,54 +1,32 @@
 import random
 
+
 def get_min_max(ints):
     if len(ints) is 0:
-        return -1
+        return "Please pass a non-empty list as an argument."
 
-    pivot = ints[len(ints) // 2]
-    smaller = []
-    larger = []
+    min_elem = ints[0]
+    max_elem = ints[0]
+
     for elem in ints:
-        if elem < pivot:
-            smaller.append(elem)
-        if elem > pivot:
-            larger.append(elem)
+        if elem < min_elem:
+            min_elem = elem
+        if elem > max_elem:
+            max_elem = elem
 
-    return get_min(smaller), get_max(larger)
-
-
-def get_max(ints):
-    pivot = ints[len(ints) // 2]
-    larger = []
-    for elem in ints:
-        if elem > pivot:
-            larger.append(elem)
-
-    if len(larger) is 0:
-        return pivot
-    else:
-        return get_max(larger)
+    return min_elem, max_elem
 
 
-def get_min(ints):
-    pivot = ints[len(ints) // 2]
-    smaller = []
-    for elem in ints:
-        if elem < pivot:
-            smaller.append(elem)
-
-    if len(smaller) is 0:
-        return pivot
-    else:
-        return get_min(smaller)
-
-
-# Example Test Case of Ten Integers
+# Test Case of Ten Random Integers
 random_ints = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(random_ints)
-
-print("Pass" if (9 == get_max(random_ints)) else "Fail")
-print("Pass" if (0 == get_min(random_ints)) else "Fail")
 print("Pass" if ((0, 9) == get_min_max(random_ints)) else "Fail")
 
-# Example Test Case of Empty List
-print("Pass" if (-1 == get_min_max([])) else "Fail")
+# Test Case for Typical Array of Numbers
+print("Pass" if ((3, 50) == get_min_max([10, 3, 27, 8, 11, 10, 50, 33])) else "Fail")
+
+# Test Case of List with One Element
+print ("Pass" if ((1, 1) == get_min_max([1])) else "Fail")
+
+# Test Case of Empty List
+print("Pass" if ("Please pass a non-empty list as an argument." == get_min_max([])) else "Fail")
